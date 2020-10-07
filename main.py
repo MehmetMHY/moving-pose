8d5298d06bede5734adcedefe5e1dc47221c648d
MehmetMHY <mehmet.mhy@gmail.com>
2020-10-07 15:51:10 -0600

    added a way to find R

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import get_data as gd
 import Skeleton as s
-
-
-train = gd.loadData("train.p")
-
-temp = train["a04_s06_e02_skeleton_proj.txt"]
+import numpy as np
+import math
 
 def printList(items):
     p = 0
     for i in range(len(items)):
-        for p in range(len(items[i])):
-            #print(len(items[i][p]))
-            print(items[i][p])
-            # if(len(items[i][p]) != 5):
-            #     print("FUCK")
-        p = p + 1
-    print("------->", p, i)
+        print(items[i])
 
-for key, value in train.items():
-    if(key == "a04_s06_e02_skeleton_proj.txt"):
-        #print(key, ' : ', value)
-        printList(value)
+# find distance between 2 3D points
+def distance(point1, point2): 
+    x1 = point1[0] ; y1 = point1[1] ; z1 = point1[1]
+    x2 = point2[0] ; y2 = point2[1] ; z2 = point2[2]
+       
+    d = math.sqrt(math.pow(x2 - x1, 2) +
+                math.pow(y2 - y1, 2) +
+                math.pow(z2 - z1, 2)* 1.0)
+    return d
+
+def avg_dis_r(data, start_to_end):
+    final = []
+    for key, value in data.items():
+        person = []
+        for i in range(len(value)):
+            frames = []
+            for j in range(len(start_to_end)):
+                p1 = start_to_end[j][0]-1
+                p2 = start_to_end[j][1]-1
+                f1 = value[i][p1][2:]
+                f2 = value[i][p2][2:]
+                frames.append(distance(f1, f2))
+            person.append(frames)
+        person = np.array(person)
+        final.append(np.mean(person, axis=0))
+
+    final = np.array(final)
+    return np.mean(final, axis=0)
+
+start_to_end = [(1, 13), (1, 2), (1, 17), (13, 14), (2, 3), (17, 18), (14, 15), (3, 5), (3, 4), (3, 9), (18, 19), (15, 16), (5, 6), (9, 10), (19, 20), (6, 7), (10, 11), (7, 8), (11, 12)]
+train = gd.loadData("train.p")
+R = list(avg_dis_r(train, start_to_end))
+print(len(R))
+
+
+
