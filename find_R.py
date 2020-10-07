2b15c173137dc5ffda3fec46b43c49449fea23c7
z3ht <andr3w.darling@gmail.com>
2020-10-07 16:38:26 -0600

    Refactor distance

--- find_R.py
+++ find_R.py
@@ -1,19 +1,14 @@
-import get_data as gd
 import Skeleton as s
 import numpy as np
-import math
+
 
 # find distance between 2 3D points
 def distance(point1, point2):
-    x1 = point1[0] ; y1 = point1[1] ; z1 = point1[1]
-    x2 = point2[0] ; y2 = point2[1] ; z2 = point2[2]
-
-    d = math.sqrt(math.pow(x2 - x1, 2) +
-                math.pow(y2 - y1, 2) +
-                math.pow(z2 - z1, 2)* 1.0)
+    d = np.sqrt(np.square(point1 - point2))
     return d
 
-def avg_dis_r(data, start_to_end=[(1, 13), (1, 2), (1, 17), (13, 14), (2, 3), (17, 18), (14, 15), (3, 5), (3, 4), (3, 9), (18, 19), (15, 16), (5, 6), (9, 10), (19, 20), (6, 7), (10, 11), (7, 8), (11, 12)]):
+
+def avg_dis_r(data):
     final = []
     for person, frames_poses in data.items():
         people = []
