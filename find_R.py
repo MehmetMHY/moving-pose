a84c9cba6c88ea9325ff6cadf1fa1e93405d7244
ehayes2000 <eqhayes@protonmail.com>
2020-10-07 16:22:46 -0600

    remove swap file

--- find_R.py
+++ find_R.py
@@ -9,16 +9,16 @@ def printList(items):
         print(items[i])
 
 # find distance between 2 3D points
-def distance(point1, point2): 
+def distance(point1, point2):
     x1 = point1[0] ; y1 = point1[1] ; z1 = point1[1]
     x2 = point2[0] ; y2 = point2[1] ; z2 = point2[2]
-       
+
     d = math.sqrt(math.pow(x2 - x1, 2) +
                 math.pow(y2 - y1, 2) +
                 math.pow(z2 - z1, 2)* 1.0)
     return d
 
-def avg_dis_r(data, start_to_end):
+def avg_dis_r(data, start_to_end=[(1, 13), (1, 2), (1, 17), (13, 14), (2, 3), (17, 18), (14, 15), (3, 5), (3, 4), (3, 9), (18, 19), (15, 16), (5, 6), (9, 10), (19, 20), (6, 7), (10, 11), (7, 8), (11, 12)]):
     final = []
     for key, value in data.items():
         person = []
@@ -36,11 +36,3 @@ def avg_dis_r(data, start_to_end):
 
     final = np.array(final)
     return np.mean(final, axis=0)
-
-start_to_end = [(1, 13), (1, 2), (1, 17), (13, 14), (2, 3), (17, 18), (14, 15), (3, 5), (3, 4), (3, 9), (18, 19), (15, 16), (5, 6), (9, 10), (19, 20), (6, 7), (10, 11), (7, 8), (11, 12)]
-train = gd.loadData("train.p")
-R = list(avg_dis_r(train, start_to_end))
-print(len(R))
-
-
-
