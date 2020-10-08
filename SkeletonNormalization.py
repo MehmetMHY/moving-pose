940f7a7ce5f5c50633bca8fab941e6493190fdd4
z3ht <andr3w.darling@gmail.com>
2020-10-07 18:32:32 -0600

    Fix norm bug

--- SkeletonNormalization.py
+++ SkeletonNormalization.py
@@ -23,28 +23,37 @@ def normalize_segment(start, start_norm, end, r):
 def normalize_skeleton(joint_data, R):
     joints = zero_to_hip(joint_data)  # TODO check if hip should always be at (0, 0, 0)
     skele = Skeleton(joints)
-    norm_joints = [skele.get_joints()[1]]  # list of normalized joint locations
+    norm_joints = [skele.get_joints()[1]]
+    reg_to_norm_dict = {
+        str(skele.get_joints()[1]): skele.get_joints()[1]
+    }
+
     for joints, r in zip(skele, R):
         start, end = joints
-        norm_joints.append(normalize_segment(start, norm_joints[-1], end, r))
-    return np.full((20, 3), norm_joints)
+        norm_end_joint = normalize_segment(start, reg_to_norm_dict[str(start)], end, r)
+        reg_to_norm_dict[end] = norm_end_joint
+        norm_joints.append(norm_end_joint)
+
+    return np.array(norm_joints).reshape((20, 3))
+
+
+import get_data as gd
+from find_R import *
+
+train = gd.loadData('train.p')
+
+# a08_s01_e01_skeleton_proj.txt         hands raised over head
+# a10_s06_e02_skeleton_proj.txt         wave
+action_data = train["a08_s01_e01_skeleton_proj.txt"]
+print(np.array(action_data).shape)
+print(np.array(action_data)[0,:,:].shape) # frame 1
+
+def get_frame(frame, data):
+    return np.array(data)[frame - 1,:,1:]
+
+test_frame = get_frame(1, action_data)
+R = avg_dis_r(train)
 
 
 
-# import get_data as gd
-# from find_R import*
-#
-# train = gd.loadData('train.p')
-#
-# # a08_s01_e01_skeleton_proj.txt         hands raised over head
-# # a10_s06_e02_skeleton_proj.txt         wave
-# action_data = train["a08_s01_e01_skeleton_proj.txt"]
-# print(np.array(action_data).shape)
-# print(np.array(action_data)[0,:,:].shape) # frame 1
-#
-# def get_frame(frame, data):
-#     return np.array(data)[frame - 1,:,1:]
-#
-# test_frame = get_frame(1, action_data)
-# R = avg_dis_r(test_frame)
-# print(normalize_skeleton(test_frame, R))
+print(normalize_skeleton(test_frame, R))
