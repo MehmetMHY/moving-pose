1e02418c32bcb868fe745bd61d35bfe595c9a59e
Andrew <andrew@andrewdarling.xyz>
2020-10-12 19:37:25 -0600

    Finish algorithm 2

--- estimator/classifiers.py
+++ estimator/classifiers.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
+
 from sklearn.base import BaseEstimator
+from sklearn.exceptions import NotFittedError
+
+from logic.metrics import max_class_score
 
 
 class ActionClassifier(BaseEstimator):
 
-    def __init__(self, descriptor_neighbors_estimator, n=5):
+    def __init__(self, descriptor_neighbors_estimator, theta=0.3, n=5):
         """
         Initialize action classifier
 
@@ -11,11 +16,13 @@ class ActionClassifier(BaseEstimator):
         ----------
         :param descriptor_neighbors_estimator: kNN classifier used for retrieving k nearest action descriptors
                     must contain:
-                        fit(X): fits the model with relevant descriptors
-                        k_descriptors(X): returns an enumerable of descriptors and their scores
+                        fit(X, y): fits the model with relevant descriptors
+                        k_descriptors(X): returns an enumerable of actions nearby X and their scores
+        :param theta: minimum score to return an action
         :param n: minimum number of frames before making a prediction
         """
         self.action_neighbors_estimator = descriptor_neighbors_estimator
+        self.theta = theta
         self.n = n
 
     def fit(self, actions, action_labels, actions_are_normalized=True):
@@ -24,7 +31,7 @@ class ActionClassifier(BaseEstimator):
 
         Parameters
         ----------
-        :param actions: Ordered list of descriptors making up an action
+        :param actions: Ordered lists of descriptors making up actions
         :param action_labels: array of labels denoting the type of action
         :param actions_are_normalized: boolean denoting whether or not actions are normalized
 
@@ -32,19 +39,53 @@ class ActionClassifier(BaseEstimator):
         -------
         :return: self
         """
+
+        if not actions_are_normalized:
+            raise NotImplemented("Actions must be normalized before they can be used to fit the Action Classifier")
+
+        descriptors = []
+        descriptor_labels = []
+        for action, label in zip(actions, action_labels):
+            descriptor_labels.extend([label] * len(action))
+            descriptors.extend(action)
+        self.action_neighbors_estimator.fit(descriptors, descriptor_labels)
         return self
 
-    def predict(self, descriptors, descriptors_are_normalized=True):
+    def predict(self, action=None, descriptors_are_normalized=True, key=None):
         """
         Predict action from descriptors
 
         Parameters
         ----------
-        :param descriptors: ordered list of descriptors describing a pose
+        :param action: ordered list of descriptors (temporal!) describing an action (required when key is None)
         :param descriptors_are_normalized: boolean denoting whether or not descriptors are normalized
+        :param key: function that retrieves next descriptor when called (required when descriptors is None)
+                        key must return temporal location or 'None' to terminate
+
+        Note: When action and key are provided, action is used
 
         Returns
         -------
-        :return: Predicted action
+        :return: predicted action as iterable
         """
-        pass
+
+        if action is None and key is None:
+            raise ValueError("Value for action or key is required when predicting an action with the Action Classifier")
+
+        if not self.action_neighbors_estimator.is_fit:
+            raise NotFittedError("The Action Classifier is not fit")
+
+        if not descriptors_are_normalized:
+            raise NotImplemented("Actions must be normalized before predictions can be made with the Action Classifier")
+
+        if action is not None:
+            action = iter(action)
+
+        class_score = defaultdict(float)
+        while (descriptor := next(action, None) if action is not None else key()) is not None:
+            for train_action, score in self.action_neighbors_estimator.k_descriptors(descriptor):
+                class_score[train_action] += score
+            mcs = max_class_score(class_score, return_total=True)
+            if mcs[0][1]/mcs[1] > self.theta:
+                yield mcs[0][0]
+        yield max_class_score(class_score)[0]
